2b39adb65407d6a79a80dd40164a096e0367e004
hanchong <chonghand@isoftstone.com>
2020-03-09 10:44:20 +0800

    搭建

---
+++ mall/mall-portal/src/main/java/com/mall/portal/controller/ss.java
@@ -0,0 +1,10 @@
+package com.mall.portal.controller;
+
+/**
+ * @author hc
+ * @classname ss
+ * @description TODO
+ * @date 2020/3/9
+ */
+public class ss {
+}
---
+++ mall/mall-portal/src/main/java/com/mall/portal/entity/jkkj.java
@@ -0,0 +1,10 @@
+package com.mall.portal.entity;
+
+/**
+ * @author hc
+ * @classname jkkj
+ * @description TODO
+ * @date 2020/3/9
+ */
+public class jkkj {
+}
