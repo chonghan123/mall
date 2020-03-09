ed688d766aad0ebf93fe4771bf3e7decd2dd70e6
hanchong <chonghand@isoftstone.com>
2020-03-09 14:24:02 +0800

    no message

--- mall/mall-admin/src/main/java/com/mall/admin/controller/UserManagerController.java
+++ mall/mall-admin/src/main/java/com/mall/admin/controller/UserManagerController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/admin/")
-public class AdminController {
+public class UserManagerController {
 
     @RequestMapping("login")
     public void login() {
