63d1ed6f41c9e828fac8e99a9813baae43fe7fe5
hanchong <chonghand@isoftstone.com>
2020-03-09 14:18:06 +0800

    打包

--- mall/mall-admin/src/main/java/com/mall/admin/controller/AdminController.java
+++ mall/mall-admin/src/main/java/com/mall/admin/controller/AdminController.java
@@ -1,16 +1,17 @@
 package com.mall.admin.controller;
 
+import common.api.Result;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 @RequestMapping("/admin/")
 public class AdminController {
 
-    @RequestMapping("test")
-    public void test() {
+    @RequestMapping("login")
+    public void login() {
 
         System.out.println(111);
-
     }
 }
---
+++ mall/mall-common/src/main/java/common/api/Result.java
@@ -0,0 +1,73 @@
+package common.api;
+
+import lombok.Data;
+
+@Data
+public class Result<T> {
+
+    private Integer code;
+
+    private String message;
+
+    private T data;
+
+    public Result() {
+
+    }
+
+    public Result(Integer code, String message, T data) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+
+    public static <T> Result<T> success(T data) {
+
+        return new Result<T>(ResultCode.SUCCESS.getCode(), ResultCode.SUCCESS.getMessage(), data);
+    }
+
+    /**
+     * 成功返回结果
+     *
+     * @param data    获取的数据
+     * @param message 提示信息
+     */
+    public static <T> Result<T> success(T data, String message) {
+        return new Result<T>(ResultCode.SUCCESS.getCode(), message, data);
+    }
+
+
+    public static <T> Result<T> failed(T data) {
+
+        return new Result<T>(ResultCode.FAILED.getCode(), ResultCode.FAILED.getMessage(), data);
+    }
+
+
+    /**
+     * 失败返回结果
+     *
+     * @param errorCode 错误码
+     */
+    public static <T> Result<T> failed(IErrorCode errorCode) {
+        return new Result<T>(errorCode.getCode(), errorCode.getMessage(), null);
+    }
+
+    /**
+     * 失败返回结果
+     *
+     * @param message 提示信息
+     */
+    public static <T> Result<T> failed(String message) {
+        return new Result<T>(ResultCode.FAILED.getCode(), message, null);
+    }
+
+    /**
+     * 失败返回结果
+     */
+    public static <T> Result<T> failed() {
+        return failed(ResultCode.FAILED);
+    }
+
+
+}
+
